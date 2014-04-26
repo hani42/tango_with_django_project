6b98a5f4d788da10b7fe39f014a393882ce670a3
hani42 <sayyid.hani@gmail.com>
2014-04-26 13:22:12 +0200

    final commit

--- rango/views.py
+++ rango/views.py
@@ -4,4 +4,4 @@ def index(request):
 	return HttpResponse("Rango says hello world! <a href='/rango/about'>About</a>")
 	
 def about(request):
-	return HttpResponse("Rango says here's the about page! and fuck you! <a href='/rango/'>Index</a>.")
+	return HttpResponse("Rango says here's the about page! and fuck you bitch! <a href='/rango/'>Index</a>.")
